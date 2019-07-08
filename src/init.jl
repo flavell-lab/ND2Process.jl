4ab0d75640cef74dae78e285922adc782100c8e8
Jungsoo Kim <jungsoo@mit.edu>
2019-07-08 12:57:08 -0400

    Doc

--- src/init.jl
+++ src/init.jl
@@ -1,5 +1,6 @@
 const py_nd2reader = PyNULL()
 
 function __init__()
+    # import nd2reader (https://github.com/JuliaPy/PyCall.jl)
     copy!(py_nd2reader, pyimport_conda("nd2reader", "nd2reader", "conda-forge"))
 end
