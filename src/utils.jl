790c8531a810b290f6b4a724b09b76e63252ab97
Jungsoo Kim <jungsoo@mit.edu>
2019-10-01 18:04:24 -0400

    remove normalization in binning

--- src/utils.jl
+++ src/utils.jl
@@ -18,7 +18,7 @@ end
 
 function bin_img(data)
     (data[1:2:end-1,1:2:end-1] .+ data[2:2:end,2:2:end] .+
-        data[1:2:end-1,2:2:end] .+ data[2:2:end,1:2:end-1]) ./ 4
+        data[1:2:end-1,2:2:end] .+ data[2:2:end,1:2:end-1])
 end
 
 function bin_img(data,n)
